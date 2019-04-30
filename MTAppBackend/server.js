e8a1f37c15913791272724ded0a06fbfaf78ecc5
Andrew Lunde <andrew.lunde@sap.com>
2019-04-30 11:39:28 -0400

    hdbext

--- MTAppBackend/server.js
+++ MTAppBackend/server.js
@@ -3,6 +3,7 @@
 
 var xsenv = require("@sap/xsenv");
 var xssec = require("@sap/xssec");
+var hdbext = require("@sap/hdbext");
 var express = require("express");
 var passport = require("passport");
 var stringifyObj = require("stringify-object");
@@ -36,10 +37,59 @@ app.get("/", function (req, res) {
 });
 
 	var responseStr = "<!DOCTYPE HTML><html><head><title>MTApp</title></head><body><h1>MTApp</h1><h2>Welcome " + req.authInfo.userInfo.givenName +
-		" " + req.authInfo.userInfo.familyName + "!</h2><p><b>Subdomain:</b> " + req.authInfo.subdomain + "</p><p><b>Identity Zone:</b> " + req.authInfo
+		" " + req.authInfo.userInfo.familyName + "!</h2><p><b>Subdomain:</b> " + req.authInfo.subdomain + "</p><br /><a href="/get_legal_entity">/get_legal_entity</a><br /><p><b>Identity Zone:</b> " + req.authInfo
 		.identityZone + "</p><p><b>Origin:</b> " + req.authInfo.origin + "</p>" + "<br /><br /><pre>" + reqStr + "</pre>" + "</body></html>";
 	res.status(200).send(responseStr);
 });
+
+app.get("/get_legal_entity", function (req, res) {
+	var reqStr = stringifyObj(req.authInfo.userInfo, {
+		indent: "   ",
+		singleQuotes: false
+	});
+
+	reqStr += "\n\n";
+
+	
+	reqStr += stringifyObj(req.authInfo.scopes, {
+		indent: "   ",
+		singleQuotes: false
+	});
+	
+	var services = xsenv.getServices({
+		hana: { tag: 'hana' }
+	});
+	
+	var svcsStr = stringifyObj(services, {
+		indent: "   ",
+		singleQuotes: false
+	});
+
+	reqStr += "\n\n";
+
+	reqStr += svcsStr;
+	
+	reqStr += "\n\n";
+
+	//SELECT * FROM "LEGAL_ENTITY"
+/*
+	var hanaConfig = {
+			host : 'hostname',
+			port : 30015,
+			user : 'user',
+			password : 'secret'
+			};
+			hdbext.createConnection(hanaConfig, function(error, client) {
+			if (error) {
+			return console.error(error);
+			}
+			client.exec(...);
+			});	
+*/	
+	var responseStr = "<!DOCTYPE HTML><html><head><title>MTApp</title></head><body><h1>MTApp Legal Entities</h1><h2>Legal Entities</h2><p><pre>" + reqStr + "</pre>" + "<br /> <a href="/">Back</a><br /></body></html>";
+	res.status(200).send(responseStr);
+});
+
 
 // subscribe/onboard a subscriber tenant
 app.put("/callback/v1.0/tenants/*", function (req, res) {
